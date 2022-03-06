47c3533b623505931e9f6641cbb315f3b24f404e
Biniam <bin_mil@yahoo.com>
2022-03-06 00:23:01 -0700

    add Tigrinya to Language Tool

---
+++ languagetool-language-modules/ti/src/main/java/org/languagetool/language/tagger/TigrinyaTagger.java
@@ -0,0 +1,7 @@
+package org.languagetool.language.tagger;
+import org.languagetool.tagging.xx.DemoTagger;
+/**
+ * Created by biniamg on 3/6/2022.
+ */
+public class TigrinyaTagger extends DemoTagger {
+}

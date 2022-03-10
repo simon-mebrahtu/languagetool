aac71e357f08673846131237fea74fd710851c65
Biniam <bin_mil@yahoo.com>
2022-03-09 17:46:49 -0700

    New changelist

--- languagetool-language-modules/ti/src/main/java/org/languagetool/language/Tigrinya.java
+++ languagetool-language-modules/ti/src/main/java/org/languagetool/language/Tigrinya.java
@@ -1,28 +1,13 @@
-/* LanguageTool, a natural language style checker
- * Copyright (C) 2014 Daniel Naber (http://www.danielnaber.de)
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
- * USA
+/**
+ * Created by biniamg on 3/6/2022.
  */
 package org.languagetool.language;
 
-import org.jetbrains.annotations.NotNull;
 import org.languagetool.Language;
 import org.languagetool.UserConfig;
 import org.languagetool.rules.*;
 import org.languagetool.tagging.Tagger;
+import org.languagetool.language.tagger.TigrinyaTagger;
 import org.languagetool.tokenizers.SRXSentenceTokenizer;
 import org.languagetool.tokenizers.SentenceTokenizer;
 
@@ -50,10 +35,9 @@ public class Tigrinya extends Language {
         return new SRXSentenceTokenizer(this);
     }
 
-    @NotNull
     @Override
     public Tagger createDefaultTagger() {
-        return new TamilTagger();
+        return new TigrinyaTagger();
     }
 
     @Override

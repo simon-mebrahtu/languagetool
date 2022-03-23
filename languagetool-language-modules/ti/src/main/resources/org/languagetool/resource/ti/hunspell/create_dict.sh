104a24aaa7e07b4a2417e60f765258dfe30a4989
Biniam <bin_mil@yahoo.com>
2022-03-23 15:10:32 -0600

    updated dictionary

--- languagetool-language-modules/ti/src/main/resources/org/languagetool/resource/ti/hunspell/create_dict.sh
+++ languagetool-language-modules/ti/src/main/resources/org/languagetool/resource/ti/hunspell/create_dict.sh
@@ -13,7 +13,7 @@ if [ $# -ne 2 ]
 then
   SCRIPT=`basename $0`
   echo "Usage: $SCRIPT <langCode> <countryCode>"
-  echo "  For example: $SCRIPT de AT"
+  echo "  For example: $SCRIPT ti ER"
   exit 1
 fi
 
@@ -21,50 +21,23 @@ REPO=$HOME/.m2/repository
 LT_VERSION=5.7-SNAPSHOT
 INPUT_ENCODING=utf8
 OUTPUT_ENCODING=utf8
-TEMP_FILE=/tmp/lt-dictionary.dump
-FINAL_FILE=/tmp/lt-dictionary.new
 
 CPATH=$REPO/com/carrotsearch/hppc/0.8.2/hppc-0.8.2.jar:$REPO/org/carrot2/morfologik-fsa-builders/2.1.7/morfologik-fsa-builders-2.1.7.jar:$REPO/org/carrot2/morfologik-stemming/2.1.7/morfologik-stemming-2.1.7.jar:$REPO/org/carrot2/morfologik-fsa/2.1.7/morfologik-fsa-2.1.7.jar:$REPO/org/carrot2/morfologik-tools/2.1.7/morfologik-tools-2.1.7.jar:$REPO/commons-cli/commons-cli/1.4/commons-cli-1.4.jar:languagetool-tools/target/languagetool-tools-${LT_VERSION}.jar
 LANG_CODE=$1
 COUNTRY_CODE=$2
 PREFIX=${LANG_CODE}_${COUNTRY_CODE}
-TOKENIZER_LANG=${LANG_CODE}-${COUNTRY_CODE}
 LANG_MAIN_DIR=languagetool-language-modules/${LANG_CODE}/src/main/resources/org/languagetool/resource/$LANG_CODE
 CONTENT_DIR=${LANG_MAIN_DIR}/hunspell
 INFO_FILE=${LANG_MAIN_DIR}/${PREFIX}.info
-FREQ_FILE=${CONTENT_DIR}/${PREFIX}_wordlist.info
-DIC_NO_SUFFIX=$CONTENT_DIR/$PREFIX
-DIC_FILE=$DIC_NO_SUFFIX.dic
-OUTPUT_FILE=${LANG_MAIN_DIR}/ti_ER.dict
+FREQ_FILE=${CONTENT_DIR}/${PREFIX}_wordlist.xml
+DIC_FILE=${CONTENT_DIR}/${PREFIX}.dic
+TEMP_FILE=${PREFIX}.text
+OUTPUT_FILE=${LANG_MAIN_DIR}/${PREFIX}.dict
+echo  $CPATH
 
-if [ ! -f $ADDITIONAL_DICT_FILE ]; then
-    echo "File not found: $ADDITIONAL_DICT_FILE"
-    exit
-fi
-if [ ! -f $UNKNOWN_TO_HUNSPELL ]; then
-    echo "File not found: $UNKNOWN_TO_HUNSPELL"
-    exit
-fi
-
-echo "Using $CONTENT_DIR/$PREFIX.dic and affix $CONTENT_DIR/$PREFIX.aff..."
-
-#mvn clean package -DskipTests &&
- unmunch $DIC_FILE $CONTENT_DIR/$PREFIX.aff | \
- # unmunch doesn't properly work for languages with compounds, thus we filter
- # the result using hunspell:
- recode $INPUT_ENCODING..$OUTPUT_ENCODING | grep -v "^#" | hunspell -d $DIC_NO_SUFFIX -G -l >$TEMP_FILE
-
-echo "Input sizes:"
-wc -l $TEMP_FILE
- 
-# remove the words that hunspell wouldn't accept (see https://github.com/languagetool-org/languagetool/issues/725#issuecomment-312961626):
-comm -23 /tmp/additional_dict_file_sorted /tmp/unknown_to_hunspell_sorted >/tmp/additional_without_hunspell_unknown
-
-cat /tmp/additional_without_hunspell_unknown $TEMP_FILE | sed 's/\r//' | sort | uniq >$FINAL_FILE
-echo "Final size:"
-wc -l $FINAL_FILE
- 
 java -cp $CPATH:languagetool-standalone/target/LanguageTool-$LT_VERSION/LanguageTool-$LT_VERSION/languagetool.jar:languagetool-standalone/target/LanguageTool-$LT_VERSION/LanguageTool-$LT_VERSION/libs/languagetool-tools.jar \
-  org.languagetool.tools.SpellDictionaryBuilder -i $FINAL_FILE -info $INFO_FILE -o $OUTPUT_FILE  -freq
+  org.languagetool.tools.SpellDictionaryBuilder -i $DIC_FILE -info $INFO_FILE -o $OUTPUT_FILE  -freq $FREQ_FILE
 
-rm $TEMP_FILE
+# Test the compiled dict by exporting it back to a text file (output text file is written in the current directory
+#java -cp $CPATH:languagetool-standalone/target/LanguageTool-$LT_VERSION/LanguageTool-$LT_VERSION/languagetool.jar:languagetool-standalone/target/LanguageTool-$LT_VERSION/LanguageTool-$LT_VERSION/libs/languagetool-tools.jar \
+#  org.languagetool.tools.DictionaryExporter -i $OUTPUT_FILE -info $INFO_FILE -o $TEMP_FILE

30ebca451a4e4ad088091846e4842ee3b2137416
Biniam <bin_mil@yahoo.com>
2022-10-04 17:23:49 -0600

    do not spell check  words written in latin  words.

--- languagetool-language-modules/tig/src/main/java/org/languagetool/language/tig/MorfologikTigreSpellerRule.java
+++ languagetool-language-modules/tig/src/main/java/org/languagetool/language/tig/MorfologikTigreSpellerRule.java
@@ -7,6 +7,8 @@ import org.languagetool.rules.spelling.morfologik.MorfologikSpellerRule;
 import java.io.IOException;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MorfologikTigreSpellerRule extends MorfologikSpellerRule {
   public MorfologikTigreSpellerRule(ResourceBundle messages, Language language, UserConfig userConfig, List<Language> altLanguages) throws IOException {
@@ -27,4 +29,13 @@ public class MorfologikTigreSpellerRule extends MorfologikSpellerRule {
   protected boolean isLatinScript() {
     return false;
   }
+
+  //ignore all characters except ሀ-ፚ
+  private static Pattern he2fie = Pattern.compile("[\\x{1200}-\\x{135A}]{1,20}") ;
+  @Override
+  protected boolean ignoreWord(String word) throws IOException {
+    Matcher matcher = he2fie.matcher(word);
+    return super.ignoreWord(word) || !matcher.matches();
+  }
+
 }
